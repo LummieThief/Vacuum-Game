6da42cb9ddaa6e435ce5afa39999b3e24b790105
jtcook <jtcook2000@gmail.com>
2022-08-13 20:03:56 -0700

    resolved merge conflict and created Dilara scene

---
+++ Vacuum Game/Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour
+{
+    public void PlayEnterSound()
+	{
+		Debug.Log("mouse entered");
+	}
+
+	public void PlayPressSound()
+	{
+		Debug.Log("pressed");
+	}
+}
